d049e53ad39cbb4064786ed32b6912ddea6a5c35
mizzadnan <mirza.adnan2205@gmail.com>
2021-01-13 13:18:36 +0600

    added height media queries

--- script.js
+++ script.js
@@ -135,6 +135,10 @@ scissors.addEventListener("click", function(){
         computer_score_display.textContent = String(computer_score)
         
     }
+
+    rock.addEventListener("transitionend", function(){
+        rock.classList.remove("pop")
+    })
 });
 
 
